427322bc2b4f32b39dfbfd3d0ddbd0c7332fd748
DehengYang <1660542154@qq.com>
2020-03-29 07:13:18 -0700

    modify & re-organize the scripts (with an example of Closure 103)

--- runGZoltar.sh
+++ runGZoltar.sh
@@ -1,72 +1,60 @@
 SCRIPT_DIR=`pwd`
 source "$SCRIPT_DIR/pathUtil.sh" || exit 1
 
-# GZoltar Version
-GZoltarVersion=1.7.3-SNAPSHOT
-gzPath=/home/apr/apr_tools
+JUNIT_JAR=lib/junit-4.12.jar
+HAMCREST_JAR=lib/hamcrest-core-1.3.jar
 
-# Absolute path of junit.jar
-JUNIT_JAR=$gzPath/gzoltar/com.gzoltar.core/target/dependency/junit-4.12.jar
-# Absolute path of hmacrest-core.jar
-HAMCREST_JAR=$gzPath/gzoltar/com.gzoltar.core/target/dependency/hamcrest-core-1.3.jar
-# Absolute path of com.gzoltar.cli-${GZoltarVersion}-jar-with-dependencies.jar
-GZOLTAR_CLI_JAR=$gzPath/gzoltar/com.gzoltar.cli/target/com.gzoltar.cli-${GZoltarVersion}-jar-with-dependencies.jar
-# Absolute path of com.gzoltar.agent.rt-${GZoltarVersion}-all.jar
-GZOLTAR_AGENT_JAR=$gzPath/gzoltar/com.gzoltar.agent.rt/target/com.gzoltar.agent.rt-${GZoltarVersion}-all.jar
+gz_version=1.7.3-SNAPSHOT
 
-# dep
-# /mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/build/classes:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/build/test:
-#deps=/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/build/lib/rhino.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/lib/args4j.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/lib/junit.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/lib/json.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/lib/ant-launcher.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/lib/jarjar.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/lib/jsr305.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/lib/protobuf-java.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/lib/ant.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/lib/guava.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/lib/caja-r4314.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/lib/rhino/testsrc/org/mozilla/javascript/tests/commonjs/module/modules.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/src/
+GZOLTAR_CLI_JAR=lib/com.gzoltar.cli-${gz_version}-jar-with-dependencies.jar
+GZOLTAR_AGENT_JAR=lib/com.gzoltar.agent.rt-${gz_version}-all.jar
+
+
+#d4j bug configuration
 depJunit=/home/apr/env/mavenDownload/junit/junit/4.11/junit-4.11.jar
+
+
+##### bug settings
+pid="$1"       # Project name
+bid="$2"       # bug id
+
+bug_dir="./$pid/${pid}_${bid}" # bug directory
 
 # closure 103
 #/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_103/build/classes:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_103/build/test:
 #/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_103/src/ 
-deps=/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_103/lib/junit4-legacy.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_103/lib/protobuf_deploy.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_103/lib/google_common_deploy.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_103/lib/ant_deploy.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_103/lib/junit4-core.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_103/lib/libtrunk_rhino_parser_jarjared.jar:/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_103/lib/hamcrest-core-1.1.jar
-#/mnt/benchmarks/repairDir/Kali_Defects4J_Closure_18/lib/junit.jar
+deps=${bug_dir}/lib/junit4-legacy.jar:${bug_dir}/lib/protobuf_deploy.jar:${bug_dir}/lib/google_common_deploy.jar:${bug_dir}/lib/ant_deploy.jar:${bug_dir}/lib/junit4-core.jar:${bug_dir}/lib/libtrunk_rhino_parser_jarjared.jar:${bug_dir}/lib/hamcrest-core-1.1.jar
 
-pid="$1"       # Project name
-bid="$2"       # bug id
-
-
-
-# Output path of fault localization results.
-output_dir="/home/apr/apr_tools/FL-VS-APR/FaultLocalization/GZoltar-1.7.3/result/${pid}-${bid}"
-
-bug_dir="./$pid/${pid}_${bid}" # bug directory
-
+output_dir="result/${pid}-${bid}"
 mkdir -p $output_dir
+unit_tests_file="$output_dir/tests.txt" # all test methods.
+ser_file="$output_dir/gzoltar.ser"
+
 test_classpath="$bug_dir$(_get_test_classpath $pid $bid)"
 src_classes_dir="$bug_dir$(_get_src_classpath $pid $bid)"
 
 echo "test_classpath: $test_classpath"
 echo "src_classes_dir: $src_classes_dir"
 
-tool="developer"                      # <developer|evosuite|randoop>
-unit_tests_file="$output_dir/tests.txt" # all test methods.
-ser_file="$output_dir/gzoltar.ser"
+D4J_DIR=/mnt/recursive-repairthemall/RepairThemAll/benchmarks/defects4j
 
-<<C1
+##### collect all tests
+# :$deps
 java -cp $src_classes_dir:$test_classpath:$JUNIT_JAR:$HAMCREST_JAR:$GZOLTAR_CLI_JAR \
   com.gzoltar.cli.Main listTestMethods $test_classpath \
     --outputFile "$unit_tests_file"
-C1
 
-#<<C2
-echo "using depJunit"
 
-java -cp $src_classes_dir:$test_classpath:$depJunit:$HAMCREST_JAR:$GZOLTAR_CLI_JAR:$deps \
-  com.gzoltar.cli.Main listTestMethods $test_classpath \
-    --outputFile "$unit_tests_file"
+##### get classes to debug
+cat "$D4J_DIR/framework/projects/$pid/loaded_classes/$bid.src" | sed 's/$/:/' | sed ':a;N;$!ba;s/\n//g'  >  "${output_dir}/classes.txt"
+#cat "$D4J_DIR/framework/projects/Closure/loaded_classes/103.src" | sed 's/$/$*:/' | sed ':a;N;$!ba;s/\n//g' >> "${output_dir}/classes.txt"
+classes_to_debug=$(cat "${output_dir}/classes.txt")
 
-#exit
 
-D4J_DIR=/mnt/recursive-repairthemall/RepairThemAll/benchmarks/defects4j
-cat "$D4J_DIR/framework/projects/Closure/loaded_classes/103.src" | sed 's/$/:/' | sed ':a;N;$!ba;s/\n//g'    > "./classes.txt"
-#cat "$D4J_DIR/framework/projects/Closure/loaded_classes/103.src" | sed 's/$/$*:/' | sed ':a;N;$!ba;s/\n//g' >> "./classes.txt"
-classes_to_debug=$(cat "./classes.txt")
-
-#java -XX:MaxPermSize=4096M -javaagent:/home/apr/apr_tools/nopol-new/nopol/nopol/lib/GZoltar/com.gzoltar.agent.rt-1.7.3-SNAPSHOT-all.jar=destfile=/mnt/workingDir/Nopol_Defects4J_Closure_1/src/../gzBuild/gzoltar.ser,buildlocation=/mnt/workingDir/Nopol_Defects4J_Closure_1/src/../gzBuild,includes=$classes_to_debug,excludes="",inclnolocationclasses=false,output="FILE"
+##### collect test execution data (generate gzoltar.ser)
+echo "$pid_$bid" >> time.txt
+startTime=$(date +%s)
+echo "start time `date '+%Y%m%d %H%M%S'`"  >> time.txt
 echo "[INFO] Start: $(date)" >&2
     (cd "$tmp_dir" > /dev/null 2>&1 && \
       java -XX:MaxPermSize=4096M -javaagent:$GZOLTAR_AGENT_JAR=destfile=$ser_file,buildlocation=$src_classes_dir,includes=$classes_to_debug,excludes="",inclnolocationclasses=false,output="FILE" \
@@ -79,24 +67,16 @@ if [ $? -ne 0 ]; then
   # rm -rf "$tmp_dir"
 fi
 
-# backup
-<<C2
-echo "[INFO] Start: $(date)" >&2
-    (cd "$tmp_dir" > /dev/null 2>&1 && \
-      java -XX:MaxPermSize=4096M -javaagent:$GZOLTAR_AGENT_JAR=destfile=$ser_file,buildlocation=$src_classes_dir,inclnolocationclasses=false,output="FILE" \
-        -cp $src_classes_dir:$JUNIT_JAR:$test_classpath:$GZOLTAR_CLI_JAR:$deps \
-        com.gzoltar.cli.Main runTestMethods \
-          --testMethods "$unit_tests_file" \
-          --collectCoverage)
-if [ $? -ne 0 ]; then
-  echo "[ERROR] GZoltar runTestMethods command has failed for $pid-${bid}b version!" >&2
-  # rm -rf "$tmp_dir"
-fi
-C2
-
+endTime=$(date +%s)
+echo "end time `date '+%Y%m%d %H%M%S'`"  >> time.txt
+repairTime=$(($endTime-$startTime))
+echo -e "time cost: $repairTime s\n\n"  >> time.txt
 
 [ -s "$ser_file" ] || die "[ERROR] $ser_file does not exist or it is empty!"
 
+
+
+##### fl report
 spectra_file="$output_dir/sfl/txt/spectra.csv"
 matrix_file="$output_dir/sfl/txt/matrix.txt"
 tests_file="$output_dir/sfl/txt/tests.csv"
